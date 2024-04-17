20bea0a85158af0580b5285d6fdde623a0a6637c
Hong Yi <im@hongyi.io>
2024-04-18 02:58:41 +0800

    add neotree changes, add keybinds

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -2,4 +2,10 @@ return {
   vim.keymap.set('n', '<leader>bd', function()
     require('mini.bufremove').delete(vim.fn.bufnr(), false)
   end),
+
+  -- allow to move lines up and down
+  vim.keymap.set('n', '<A-j>', ':m .+1<CR>=='),
+  vim.keymap.set('n', '<A-k>', ':m .-2<CR>=='),
+  vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv"),
+  vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv"),
 }
--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -1,13 +1,14 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
   dependencies = {
+    version = '3.x',
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   opts = {
+    popup_border_style = 'single',
     filesystem = {
       follow_current_file = { enabled = true },
       filtered_items = {
