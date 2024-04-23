a92863259a67892eddd187ebe78afb7a44b9b00a
Hong Yi <im@hongyi.io>
2024-04-23 10:45:46 +0800

    add trouble.nvim

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -8,4 +8,23 @@ return {
   vim.keymap.set('n', '<A-k>', ':m .-2<CR>=='),
   vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv"),
   vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv"),
+  -- trouble.nvim
+  vim.keymap.set('n', '<leader>xx', function()
+    require('trouble').toggle()
+  end),
+  vim.keymap.set('n', '<leader>xw', function()
+    require('trouble').toggle 'workspace_diagnostics'
+  end),
+  vim.keymap.set('n', '<leader>xd', function()
+    require('trouble').toggle 'document_diagnostics'
+  end),
+  vim.keymap.set('n', '<leader>xq', function()
+    require('trouble').toggle 'quickfix'
+  end),
+  vim.keymap.set('n', '<leader>xl', function()
+    require('trouble').toggle 'loclist'
+  end),
+  vim.keymap.set('n', 'gR', function()
+    require('trouble').toggle 'lsp_references'
+  end),
 }
